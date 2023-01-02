b75b523f0d1ac8f9f6059b9e17436327297eeabd
김 예진 <jdoe@example.com>
2023-01-02 18:25:23 +0900

    service 문서 수정

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Movie } from './entities/movie.entity';
 
 @Injectable()
@@ -10,7 +10,11 @@ export class MoviesService {
   }
 
   getOne(id : string): Movie{ 
-    return this.movies.find(movie => movie.id === parseInt(id)); 
+    const movie = this.movies.find(movie => movie.id === parseInt(id)); 
+    if(!movie) { 
+      throw new NotFoundException("Movie with Id: " + id); 
+    }
+    return movie 
   }
 
   deleteOne(id:string): boolean{ 
